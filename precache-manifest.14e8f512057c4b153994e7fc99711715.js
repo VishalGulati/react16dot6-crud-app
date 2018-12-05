36503f9067c48fa31b6c0d13534102f079720141
Vishal Gulati <visgulat@publicisgroupe.net>
2018-12-05 18:52:27 +0530

    Updates

---
+++ precache-manifest.14e8f512057c4b153994e7fc99711715.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7bde12e52d030491c13ba2c795c430",
+    "url": "/react16dot6-crud-app/static/media/bg.8e7bde12.jpg"
+  },
+  {
+    "revision": "8f6d0c34d0f766a9e7bc",
+    "url": "/react16dot6-crud-app/static/js/runtime~main.8f6d0c34.js"
+  },
+  {
+    "revision": "9e8ae3e48ec953abfe40",
+    "url": "/react16dot6-crud-app/static/js/main.9e8ae3e4.chunk.js"
+  },
+  {
+    "revision": "ede86ed8d332909e5735",
+    "url": "/react16dot6-crud-app/static/js/1.ede86ed8.chunk.js"
+  },
+  {
+    "revision": "9e8ae3e48ec953abfe40",
+    "url": "/react16dot6-crud-app/static/css/main.9205d1a2.chunk.css"
+  },
+  {
+    "revision": "ede86ed8d332909e5735",
+    "url": "/react16dot6-crud-app/static/css/1.6c40f742.chunk.css"
+  },
+  {
+    "revision": "4a0f1fce46f2e22e9fa74ce2c6dbc653",
+    "url": "/react16dot6-crud-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.24ef930c0938d88c6b905491c6b162df.js"
+  "/react16dot6-crud-app/precache-manifest.14e8f512057c4b153994e7fc99711715.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react16dot6-crud-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
