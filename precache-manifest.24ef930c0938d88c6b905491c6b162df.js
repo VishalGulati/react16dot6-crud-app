8743d9ef865cf3315a996445fda73781e60f2b90
Vishal Gulati <visgulat@publicisgroupe.net>
2018-12-05 18:51:23 +0530

    Updates

---
+++ precache-manifest.24ef930c0938d88c6b905491c6b162df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e7bde12e52d030491c13ba2c795c430",
+    "url": "/static/media/bg.8e7bde12.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c7af2af8e79f0eb748b8",
+    "url": "/static/js/main.c7af2af8.chunk.js"
+  },
+  {
+    "revision": "ede86ed8d332909e5735",
+    "url": "/static/js/1.ede86ed8.chunk.js"
+  },
+  {
+    "revision": "c7af2af8e79f0eb748b8",
+    "url": "/static/css/main.a01b463b.chunk.css"
+  },
+  {
+    "revision": "ede86ed8d332909e5735",
+    "url": "/static/css/1.6c40f742.chunk.css"
+  },
+  {
+    "revision": "15469bef13f5ee80c06da22540b4420d",
+    "url": "/index.html"
+  }
+];
