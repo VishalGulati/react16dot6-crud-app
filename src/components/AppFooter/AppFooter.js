0f57a9cceb2505905c33cf634fbc5573eecfc6be
Vishal Gulati <visgulat@publicisgroupe.net>
2018-12-05 19:02:55 +0530

    Changed class to classname

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -5,7 +5,7 @@ class AppFooter extends Component {
     render() {
         return (
                 <footer>
-                    Designed and Coded with <span class="glyphicon glyphicon-heart"></span> by Vishal Gulati 
+                    Designed and Coded with <span className="glyphicon glyphicon-heart"></span> by Vishal Gulati 
                     (<a id="footer-link" target="_blank" rel="noopener noreferrer" href="https://www.vishalgulati.com">https://www.vishalgulati.com</a>)
                 </footer>
         );
